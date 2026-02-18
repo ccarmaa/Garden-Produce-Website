b3f286e3d1767beb22fa3514ab07072d2e699a59
carmaa <crfrencho@gmail.com>
2026-02-17 22:04:00 -0500

    fixed nav sizes and styled footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,26 +3,31 @@ import { Instagram } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="py-6 mt-auto">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between">
-          <div>
-            Beacon Street Gardens LLC
-          </div>
-
-          <div className="flex items-center space-x-4">
-            <Link href="/contact">
-              Contact
-            </Link>
-            <span>|</span>
-            <div className="flex items-center space-x-2">
-              <span>Follow Us</span>
-                <Instagram size={20} />
-
+    <footer className="mt-auto bg-[var(--footer)] border-t border-gray-300">
+        <div className="mx-auto px-8 py-5">
+            <div className="flex items-center justify-between">
+            {/*left*/}
+            <div className="text-[var(--text)] font-medium">
+                Beacon Street Gardens <span className="text-xs font-normal">LLC</span>
             </div>
-          </div>
+
+            {/*right*/}
+            <div className="flex items-center gap-4 text-[var(--text)]">
+                <Link href="/contact" className="hover:text-[var(--rust)] transition-colors ">
+                    Contact
+                </Link>
+                <span className="text-gray-400">|</span>
+                <a 
+                    href="https://www.instagram.com/crfrencho/" 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="hover:text-[var(--rust)] transition-colors flex items-center gap-3">
+                    <span>Follow Us</span>
+                    <Instagram size={20}/>
+                </a>
+            </div>
+            </div>
         </div>
-      </div>
     </footer>
   );
 }
--- components/layout/Navigation.tsx
+++ components/layout/Navigation.tsx
@@ -19,20 +19,20 @@ export default function Navigation() {
 
     return (
         <nav className="sticky top-0 z-50 bg-[var(--header)] border-b border-gray-200 shadow-sm h-[60px]">
-            <div className="mx-auto px-8 py-2 h-full flex items-center">
+            <div className="mx-auto px-8 py-4 h-full flex items-center">
                 <div className="flex items-center justify-between w-full">
                 {/*logo*/}
                 <Link href="/" className="flex items-center space-x-3 hover:opacity-80 transition-opacity">
-                        <Image src="/logo-new.png" alt="Logo" width={95} height={40} className="rounded-full" />
-                        <span className="text-[36px] font-MEDIUM text-[var(--text)]">
-                            Beacon Street Gardens <span className="text-[17px] font-normal">LLC</span>
+                        <Image src="/logo-new.png" alt="Logo" width={60} height={60}/>
+                        <span className="text-2xl font-medium text-[var(--text)]">
+                            Beacon Street Gardens <span className="text-sm font-normal">LLC</span>
                         </span>
                     </Link>
 
                 {/*nav links*/}
                 <div className="flex items-center gap-8">
                     {navLinks.map((link) => (
-                    <Link key={link.href} href={link.href} className={`relative text-[20px] font-medium transition-colors ${pathname === link.href ? 'text-[var(--rust)] ' : 'text-[var(--text)] hover:text-[var(--rust)]'}`}>
+                    <Link key={link.href} href={link.href} className={`relative text-base font-medium transition-colors ${pathname === link.href ? 'text-[var(--rust)] ' : 'text-[var(--text)] hover:text-[var(--rust)]'}`}>
                         {link.label}
                         {pathname === link.href && (
                             <span className="absolute -bottom-1 left-0 w-full h-[1.5px] bg-[var(--rust)] animate-[slideIn_0.3s_ease-in-out]" />
@@ -42,7 +42,7 @@ export default function Navigation() {
 
                     {/*cart icon*/}
                     <Link href="/cart" className=" relative text-[var(--text)] hover:text-[var(--rust)] transition-colors">
-                        <ShoppingCart size={25} />
+                        <ShoppingCart size={22} />
                     </Link>
                 </div>
 
